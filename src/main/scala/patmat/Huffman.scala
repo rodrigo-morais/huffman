b892d1edb660e5f8ecec08c9d3247c828060b4ca
Rodrigo Morais <morais.rm@gmail.com>
2016-01-15 14:50:09 -0200

    Creating the until function in Huffman tree.

--- src/main/scala/patmat/Huffman.scala
+++ src/main/scala/patmat/Huffman.scala
@@ -133,7 +133,9 @@ object Huffman {
    *    the example invocation. Also define the return type of the `until` function.
    *  - try to find sensible parameter names for `xxx`, `yyy` and `zzz`.
    */
-    def until(xxx: ???, yyy: ???)(zzz: ???): ??? = ???
+    def until(validation: List[CodeTree] => Boolean, reduction: List[CodeTree] => List[CodeTree])(trees: List[CodeTree]): List[CodeTree] = 
+      if(validation(trees)) trees
+      else until(validation, reduction)(reduction(trees))
   
   /**
    * This function creates a code tree which is optimal to encode the text `chars`.
