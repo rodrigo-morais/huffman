972b5a2b3694ee1578a0300493d80cbfc38f26a1
Rodrigo Morais <morais.rm@gmail.com>
2016-01-15 10:05:13 -0200

    Adding the function which return the leafs in order by weight.

--- src/main/scala/patmat/Huffman.scala
+++ src/main/scala/patmat/Huffman.scala
@@ -90,7 +90,8 @@ object Huffman {
    * head of the list should have the smallest weight), where the weight
    * of a leaf is the frequency of the character.
    */
-    def makeOrderedLeafList(freqs: List[(Char, Int)]): List[Leaf] = ???
+    def makeOrderedLeafList(freqs: List[(Char, Int)]): List[Leaf] =
+      freqs.sortBy(x => x._2).map(x => Leaf(x._1, x._2))
   
   /**
    * Checks whether the list `trees` contains only one single code tree.
